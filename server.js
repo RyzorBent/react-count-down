2d6072edc03cc41f51a7c8e6c1c1ec205bd6b240
RyzorBent <zestadaryzor@gmail.com>
2024-02-26 10:35:55 +0200

    remove logger

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
-  logger.info('running on heroku servers');
+  console.log('running on heroku servers');
   app.use(express.static(path.join(__dirname, 'dashboard/build')));
 
   app.get('*', (req, res) => {
-    logger.info('prod static space params --> %o', req.params);
-    logger.info('prod static space body -->  %o', req.body);
+    console.log('prod static space params --> %o', req.params);
+    console.log('prod static space body -->  %o', req.body);
     res.sendFile(path.join(__dirname, 'dashboard', 'build', 'index.html'));
   });
 }
